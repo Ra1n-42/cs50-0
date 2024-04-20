ec3ce124872697ce49aa83a2f00bac3d2a956866
Rain <150204147+Ra1n-42@users.noreply.github.com>
2024-04-20 18:41:43 +0000

    Sat, Apr 20, 2024, 8:41 PM +02:00

--- credit/credit.c
+++ credit/credit.c
@@ -10,13 +10,15 @@ int main(void)
     // int number = get_long("Number: ");
     int digit_count = count_digits(credit_number);
     char card_str[digit_count];
-    // card identification: American Express uses 15-digit numbers, MasterCard uses 16-digit numbers, and Visa uses 13- and 16-digit numbers.
 
 
     // let’s first underline every other digit, starting with the number’s second-to-last digit
     // let’s multiply each of the underlined digits by 2
     // Now let’s add those products’ digits (i.e., not the products themselves) together
     // Now let’s add that sum (13) to the sum of the digits that weren’t multiplied by 2 (starting from the end)
+
+    // card identification: American Express uses 15-digit numbers, MasterCard uses 16-digit numbers, and Visa uses 13- and 16-digit numbers.
+    // All American Express numbers start with 34 or 37; most MasterCard numbers start with 51, 52, 53, 54, or 55
     if (digit_count == 15)
     {
         printf("American Express");
