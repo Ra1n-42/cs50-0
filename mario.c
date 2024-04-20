00504bdd098c4becc610239416370b5d5fb5a07c
Rain <150204147+Ra1n-42@users.noreply.github.com>
2024-04-20 09:12:28 +0000

    Sat, Apr 20, 2024, 11:12 AM +02:00

--- mario.c
+++ mario.c
@@ -2,5 +2,13 @@
 
 int main(void)
 {
-    
+    int n = 3;
+    for (int i= 0; i < n; i++)
+    {
+        for (int j= 0; j < n; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 }
