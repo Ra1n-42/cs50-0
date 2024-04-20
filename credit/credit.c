02a7e8b184a3b742e4e64960b7f34d44cc311368
Rain <150204147+Ra1n-42@users.noreply.github.com>
2024-04-20 18:28:14 +0000

    Sat, Apr 20, 2024, 8:28 PM +02:00

--- credit/credit.c
+++ credit/credit.c
@@ -8,6 +8,11 @@ int main(void)
 {
     // int number = get_int("Number: ");
     long card = 4111111123456789;
+
+    // let’s first underline every other digit, starting with the number’s second-to-last digit
+    // let’s multiply each of the underlined digits by 2
+    // Now let’s add those products’ digits (i.e., not the products themselves) together
+    // Now let’s add that sum (13) to the sum of the digits that weren’t multiplied by 2 (starting from the end)
     int digit_count = count_digits(card);
     printf("Die maximale Anzahl der Ziffern in der Zahl %ld ist: %d\n", card, digit_count);
 }
