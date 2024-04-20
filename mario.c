b4286bca1e305008f0923ae6cd7f30d182ba8227
Rain <150204147+Ra1n-42@users.noreply.github.com>
2024-04-20 09:17:22 +0000

    Sat, Apr 20, 2024, 11:17 AM +02:00

--- mario.c
+++ mario.c
@@ -3,12 +3,11 @@
 
 int main(void)
 {
-    int n = get_int("Size: ");
-
-    while (n < 1)
+    do
     {
         n = get_int("Size: ");
     }
+    while (n < 1);
 
 
     for (int i= 0; i < n; i++)
