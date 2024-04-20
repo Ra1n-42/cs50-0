b913c23e2c6982ed000cd49f2bafba3f2e8bb0a3
Rain <150204147+Ra1n-42@users.noreply.github.com>
2024-04-20 18:23:54 +0000

    Sat, Apr 20, 2024, 8:23 PM +02:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,11 +1,23 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <math.h>
 
+int count_digits(long num);
 
 int main(void)
 {
     // int number = get_int("Number: ");
     long card = 4111111123456789;
+    int digit_count = count_digits(card);
+    printf("Die maximale Anzahl der Ziffern in der Zahl %ld ist: %d\n", card, digit_count);
+}
 
-    printf("%li\n", card % 9);
+
+int count_digits(long num) {
+    int count = 0;
+    while (num != 0) {
+        num = num / 10;
+        count++;
+    }
+    return count;
 }
