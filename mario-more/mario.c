697bafe2fc6d42cc32373e65e64502bed940ce99
Rain <150204147+Ra1n-42@users.noreply.github.com>
2024-04-20 11:16:33 +0000

    Sat, Apr 20, 2024, 1:16 PM +02:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 
-void print_row(int bricks);
+void print_row(int space, int bricks);
 
 int main(void)
 {
@@ -14,15 +14,15 @@ int main(void)
     for (int i = 0; i < n; i++)
     {
         // Print row of bricks
-        print_row(i + 1);
+        print_row(n, i + 1);
     }
 }
 
-void print_row(int bricks)
+void print_row(int space, int bricks)
 {
     for (int i = 0; i < bricks; i++)
     {
-        printf("%*s #",bricks);
+        printf("%*s #",space - bricks,"");
     }
     printf("\n");
 }
