493c83a43d703c0bd45f271d2f87d7798846f9b2
Rain <150204147+Ra1n-42@users.noreply.github.com>
2024-04-20 18:39:25 +0000

    Sat, Apr 20, 2024, 8:39 PM +02:00

--- credit/credit.c
+++ credit/credit.c
@@ -9,7 +9,7 @@ int main(void)
     long credit_number = 4111111123456789;
     // int number = get_long("Number: ");
     int digit_count = count_digits(credit_number);
-    char card_str[digit_count]
+    char card_str[digit_count];
     // card identification: American Express uses 15-digit numbers, MasterCard uses 16-digit numbers, and Visa uses 13- and 16-digit numbers.
 
 
@@ -17,7 +17,18 @@ int main(void)
     // let’s multiply each of the underlined digits by 2
     // Now let’s add those products’ digits (i.e., not the products themselves) together
     // Now let’s add that sum (13) to the sum of the digits that weren’t multiplied by 2 (starting from the end)
-    printf("Die maximale Anzahl der Ziffern in der Zahl %ld ist: %d\n", credit_number, digit_count);
+    if (digit_count == 15)
+    {
+        printf("American Express");
+    }
+    else if (digit_count == 16){
+        printf("MasterCard");
+    }
+    else if (digit_count < 17 | digit_count >= 13)
+    {
+        printf("Visa");
+    }
+    printf("\n");
 }
 
 
