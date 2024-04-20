9ef33d7313acce9e59fd8ae1d60c0f9d565dd39c
Rain <150204147+Ra1n-42@users.noreply.github.com>
2024-04-20 19:12:33 +0000

    Sat, Apr 20, 2024, 9:12 PM +02:00

--- credit/credit.c
+++ credit/credit.c
@@ -33,7 +33,7 @@ int main(void)
     if (sum % 10 != 0) { // Überprüfe, ob die Summe durch 10 ohne Rest teilbar ist
         printf("INVALID.\n");
     }
-
+    if (digit_count >= 13 )
 
     // card identification: American Express uses 15-digit numbers, MasterCard uses 16-digit numbers, and Visa uses 13- and 16-digit numbers.
     // All American Express numbers start with 34 or 37; most MasterCard numbers start with 51, 52, 53, 54, or 55 and all Visa numbers start with 4.
