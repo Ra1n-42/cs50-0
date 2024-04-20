65755d3ffdb47c49baac18c96f6d51e8c5633a6b
Rain <150204147+Ra1n-42@users.noreply.github.com>
2024-04-20 18:33:25 +0000

    Sat, Apr 20, 2024, 8:33 PM +02:00

--- credit/credit.c
+++ credit/credit.c
@@ -6,15 +6,18 @@ int count_digits(long num);
 
 int main(void)
 {
+    long credit_number = 4111111123456789;
     // int number = get_long("Number: ");
-    long card = 4111111123456789;
+    int digit_count = count_digits(credit_number);
+    char card_str[digit_count]
+    // card identification: American Express uses 15-digit numbers, MasterCard uses 16-digit numbers, and Visa uses 13- and 16-digit numbers.
+
 
     // let’s first underline every other digit, starting with the number’s second-to-last digit
     // let’s multiply each of the underlined digits by 2
     // Now let’s add those products’ digits (i.e., not the products themselves) together
     // Now let’s add that sum (13) to the sum of the digits that weren’t multiplied by 2 (starting from the end)
-    int digit_count = count_digits(card);
-    printf("Die maximale Anzahl der Ziffern in der Zahl %ld ist: %d\n", card, digit_count);
+    printf("Die maximale Anzahl der Ziffern in der Zahl %ld ist: %d\n", credit_number, digit_count);
 }
 
 
