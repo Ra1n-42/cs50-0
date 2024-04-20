cb6116cb2e5a4d5afd38ae874197215197859b0e
Rain <150204147+Ra1n-42@users.noreply.github.com>
2024-04-20 08:50:41 +0000

    Sat, Apr 20, 2024, 10:50 AM +02:00

--- calculator.c
+++ calculator.c
@@ -2,19 +2,18 @@
 #include <cs50.h>
 
 
-int add(int x, int y);
+int add(int a, int b);
 
 int main(void)
 {
     int x = get_int("x: ");
     int y = get_int("y: ");
 
-    int result = add(x, y);
 
-    printf("%i\n", result);
+    printf("%i\n", add(x, y));
 }
 
-int add(int x, int y)
+int add(int a, int b)
 {
-    return x + y;
+    return a + b;
 }
