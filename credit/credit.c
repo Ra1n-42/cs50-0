3cf3e1b3d0e1c5c876503888d4f74ed74e5e792f
Rain <150204147+Ra1n-42@users.noreply.github.com>
2024-04-20 20:02:20 +0000

    Sat, Apr 20, 2024, 10:02 PM +02:00

--- credit/credit.c
+++ credit/credit.c
@@ -54,9 +54,6 @@ int main(void){
             }
         }
         if (digit_count == 15){
-            if ( first_two == 51 || first_two == 52 || first_two == 53 || first_two == 54 || first_two == 55 ){
-                printf("American Express.\n");
-            }
             if ( first_two == 37 ){
                 printf("AMEX\n");
             }
