9a046f87f4b340834b3a5ec2d1b321e1b7b23971
Rain <150204147+Ra1n-42@users.noreply.github.com>
2024-04-22 16:17:17 +0000

    Mon, Apr 22, 2024, 6:17 PM +02:00

--- week2/functions/triangle.c
+++ week2/functions/triangle.c
@@ -9,5 +9,9 @@ int main(void)
 
 bool valid_triangle(int a, int b, int c)
 {
-    
+    if (a < 1 || b < 1 || c < 1 || )
+    {
+        return false;
+    }
+    return a + b > c || b + c > a || a + c > b ;
 }
