8e868cd37d41ecd9d4b0a2a7a5b9026e66d7638b
Rain <150204147+Ra1n-42@users.noreply.github.com>
2024-04-22 22:03:38 +0000

    Tue, Apr 23, 2024, 12:03 AM +02:00

--- week3/phonebook.c
+++ week3/phonebook.c
@@ -14,15 +14,24 @@ int main(void)
 {
     // string names[] = {"Carter", "David", "John"};
     // string numbers[] = {"+1-617-495-1000", "+1-617-495-1000", "+1-949-468-2750"};
-
     person people[3];
+
+    people[0].name = "Carter";
+    people[0].number = "+1-617-495-1000";
+
+    people[1].name = "David";
+    people[1].number = "+1-617-495-1000";
+
+    people[2].name = "John";
+    people[2].number = "+1-949-468-2750";
+
     string name = get_string("Name: ");
 
     for (int i = 0; i < 3; i++)
     {
-        if (strcmp(names[i], name)==0)
+        if (strcmp(people[i].name, name)==0)
         {
-            printf("Found %s\n", numbers[i]);
+            printf("Found %s\n", people[i].number);
             return 0;
         }
     }
