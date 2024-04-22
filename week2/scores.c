6a4230cc71fc3d5b5d8d4d86c457e5802cafb1f9
Rain <150204147+Ra1n-42@users.noreply.github.com>
2024-04-22 12:38:19 +0000

    Mon, Apr 22, 2024, 2:38 PM +02:00

--- week2/scores.c
+++ week2/scores.c
@@ -21,7 +21,7 @@ int main(void)
 float average(int length, int array[])
 {
     int sum = 0;
-    for (int i = 0, i < length; i++)
+    for (int i = 0; i < length; i++)
     {
         sum += array[i];
     }
