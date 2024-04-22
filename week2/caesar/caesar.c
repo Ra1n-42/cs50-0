b9573fa8620699c6b182c23f108f781188f64880
Rain <150204147+Ra1n-42@users.noreply.github.com>
2024-04-22 19:38:48 +0000

    Mon, Apr 22, 2024, 9:38 PM +02:00

--- week2/caesar/caesar.c
+++ week2/caesar/caesar.c
@@ -10,19 +10,19 @@ int main(int argc, string argv[])
     // if only 1 argument
     if (argc != 2)
     {
-        for (int i = 0; argv[1][i] != '\0'; i++)
-        {
-            if (!isdigit((unsigned char)argv[1][i]))
-            {
-                printf("Usage: ./caesar key\n");
-                return 1;
-            }
-        }
         printf("Usage: ./caesar key\n");
         return 1;
     }
-    int k = atoi(argv[1]);
-    string text = "abc";
-    printf("%i", k);
-    printf("\n");
+    else{
+        string text = argv[1];
+        int len = strlen(text);
+        for (int i = 0; i < len; i++)
+        {
+            if(!isdigit(text[i])){
+                printf("Usage: ./caesar key\n");
+                return 1;
+            }
+            printf("%c\n", text[i]);
+        }
+    }
 }
