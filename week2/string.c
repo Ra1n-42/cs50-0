e56a518b2fc4f1bc9a50d97bd934d98b66328c3c
Rain <150204147+Ra1n-42@users.noreply.github.com>
2024-04-22 13:30:12 +0000

    Mon, Apr 22, 2024, 3:30 PM +02:00

--- week2/string.c
+++ week2/string.c
@@ -9,7 +9,7 @@ int main(void)
 
     // wrong way to do: strlen in expression is everytime executed
     // (int i = 0; i < strlen(s); i++)
-    // right way: n = strlen(s) is in the initialisation 
+    // right way: n = strlen(s) is in the initialisationsection, so its executed once
     for (int i = 0, n = strlen(s); i < n; i++)
     {
         printf("%c", s[i]);
