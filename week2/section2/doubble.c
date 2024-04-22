cc319c6c1bb5ff7d63bc7b50d240cc8b61b38189
Rain <150204147+Ra1n-42@users.noreply.github.com>
2024-04-22 15:08:34 +0000

    Mon, Apr 22, 2024, 5:08 PM +02:00

--- week2/section2/doubble.c
+++ week2/section2/doubble.c
@@ -7,10 +7,10 @@ int main(void)
     int sequence[5];
     sequence[0] = 1;
 
-    printf("%i\n", sequence[0]);
 
-    for (int i = 01; i < 5; i++)
+    for (int i = 1; i < 5; i++)
     {
-        sequence[i] -= 1
+        sequence[i] = sequence[i - 1] * 2;
+        printf("%i\n", sequence[i]);
     }
 }
