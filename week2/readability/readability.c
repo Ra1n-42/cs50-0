455deccbfeb05292ce455f38093c5ab83d0398c1
Rain <150204147+Ra1n-42@users.noreply.github.com>
2024-04-22 18:05:00 +0000

    Mon, Apr 22, 2024, 8:05 PM +02:00

--- week2/readability/readability.c
+++ week2/readability/readability.c
@@ -16,11 +16,11 @@ int main(void)
     // Count the number of letters, words, and sentences in the text
     int letters = count_letters(text);
     int words = count_words(text);
-    // int sentences = count_sentences(text);
+    int sentences = count_sentences(text);
 
     // Compute the Coleman-Liau index
 
-    printf("%d words count\n", words);
+    printf("%d sentences count\n", sentences);
 
     // Print the grade level
 }
@@ -57,9 +57,12 @@ int count_words(string text)
 
 int count_sentences(string text)
 {
+    int sentence_count = 0;
     for (int i = 0, len = strlen(text); i < len; i++)
     {
-        if (text[i] == '.' )
+        if (ispunct(text[i])) {
+            sentence_count ++;
+        }
     }
-    return 1;
+    return sentence_count;
 }
