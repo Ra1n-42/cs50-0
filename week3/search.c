5e6676b109226fd732b196761c6e3af26238be9b
Rain <150204147+Ra1n-42@users.noreply.github.com>
2024-04-22 21:43:53 +0000

    Mon, Apr 22, 2024, 11:43 PM +02:00

--- week3/search.c
+++ week3/search.c
@@ -3,13 +3,13 @@
 
 int main(void)
 {
-    int numbers[] = {20, 500, 10, 5, 100, 1, 50};
+    string strings[] = {"battleship", "boot", "cannon", "iron", "thimble", "top hat"};
 
-    int n = get_int("Number: ");
+    string s = get_string("String: ");
 
-    for (int i = 0; i < 7; i++)
+    for (int i = 0; i < 6; i++)
     {
-        if (numbers[i]==n)
+        if (strings[i] == s)
         {
             printf("found\n");
             return 0;
