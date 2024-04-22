f7883f6eb5d8683583a556610ca048c0217f938c
Rain <150204147+Ra1n-42@users.noreply.github.com>
2024-04-22 12:27:21 +0000

    Mon, Apr 22, 2024, 2:27 PM +02:00

--- week2/scores.c
+++ week2/scores.c
@@ -3,13 +3,13 @@
 
 int main(void)
 {
-    int top = 3
-    int scores[top];
-    for (int i =0; i < top; i++)
+    const int N = 3
+    int scores[N];
+    for (int i =0; i < N; i++)
     {
         scores[i] = get_int("Score: ")
     }
 
 
-    printf("Average: %f\n",(scores[0] + scores[1] + scores[2]) / 3.0);
+    printf("Average: %f\n",(scores[0] + scores[1] + scores[2]) / (float) N);
 }
