bdf890721bf8a6f9d8979e9176fef19d8da962e5
Rain <150204147+Ra1n-42@users.noreply.github.com>
2024-04-22 19:24:50 +0000

    Mon, Apr 22, 2024, 9:24 PM +02:00

--- week2/caesar/caesar.c
+++ week2/caesar/caesar.c
@@ -2,11 +2,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 
 int main(int argc, string argv[])
 {
-    if (argc != 2)
+    if (argc != 2 && isdigit(argv[1])==0)
     {
         printf("Usage: ./caesar key\n");
         return 1;
