4f70a1db80c89b89a62f1c023282d6263055cd8d
Rain <150204147+Ra1n-42@users.noreply.github.com>
2024-04-22 18:56:05 +0000

    Mon, Apr 22, 2024, 8:56 PM +02:00

--- week2/caesar/caesar.c
+++ week2/caesar/caesar.c
@@ -1,9 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-    char A = 'A';
-
-    printf("%c\n", A+1);
+    int k = 1;
+    string p = "abc";
+    for (int i = 0, len = strlen(p); i < len; i++)
+    {
+        printf("%c", p[i] + k);
+    }
+    printf("\n");
 }
+
+char cipher 
