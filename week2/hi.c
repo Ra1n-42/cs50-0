d4e96795464618b1128dc2b3d7cdd2b19ca19e48
Rain <150204147+Ra1n-42@users.noreply.github.com>
2024-04-22 12:49:55 +0000

    Mon, Apr 22, 2024, 2:49 PM +02:00

--- week2/hi.c
+++ week2/hi.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
+#include <cs50.h>
 
 int main(void)
 {
-    char c1 = 'H';
-    char c2 = 'I';
-    char c3 = '!';
+    string s = "Hi!";
 
-    printf("%i %i %i\n", c1, c2, c3);
+    printf("%s\n", s);
 }
