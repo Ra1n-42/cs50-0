e758f4b870c23b20757f761d701a5cfaa3db7b2f
Rain <150204147+Ra1n-42@users.noreply.github.com>
2024-04-22 22:00:03 +0000

    Tue, Apr 23, 2024, 12:00 AM +02:00

--- week3/phonebook.c
+++ week3/phonebook.c
@@ -3,11 +3,19 @@
 #include <string.h>
 
 
+typedef struct
+{
+    string name;
+    string number;
+}
+person;
+
 int main(void)
 {
-    string names[] = {"Carter", "David", "John"};
-    string numbers[] = {"+1-617-495-1000", "+1-617-495-1000", "+1-949-468-2750"};
+    // string names[] = {"Carter", "David", "John"};
+    // string numbers[] = {"+1-617-495-1000", "+1-617-495-1000", "+1-949-468-2750"};
 
+    person people[3];
     string name = get_string("Name: ");
 
     for (int i = 0; i < 3; i++)
