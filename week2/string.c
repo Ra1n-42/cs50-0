cb9f2b42f2871055284bd5f0963c29873ce309bc
Rain <150204147+Ra1n-42@users.noreply.github.com>
2024-04-22 13:23:57 +0000

    Mon, Apr 22, 2024, 3:23 PM +02:00

--- week2/string.c
+++ week2/string.c
@@ -5,5 +5,15 @@
 int main(void)
 {
     string s = get_string("Input: ");
-    
+    printf("Output: ");
+
+    // wrong way to do: strlen in expression is everytime executed
+    // (int i = 0; i < strlen(s); i++)
+    // right way: n = strlen(s) is in the initialisation 
+    for (int i = 0, n = strlen(s); i < n; i++)
+    {
+        printf("%c", s[i]);
+    }
+    printf("\n");
+
 }
