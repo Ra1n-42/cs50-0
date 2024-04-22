923c8761aa88f2bdf6f443420bfe021244cacda7
Rain <150204147+Ra1n-42@users.noreply.github.com>
2024-04-22 16:22:14 +0000

    Mon, Apr 22, 2024, 6:22 PM +02:00

--- week2/functions/triangle.c
+++ week2/functions/triangle.c
@@ -1,15 +1,21 @@
 #include <cs50.h>
 #include <stdio.h>
 
+bool valid_triangle(int a, int b, int c);
+
 int main(void)
 {
+    int side_a = 1;
+    int side_b = 2;
+    int side_c = 4;
 
+    printf("%c\n", valid_triangle(side_a, side_b, side_c));
 }
 
 
 bool valid_triangle(int a, int b, int c)
 {
-    if (a < 1 || b < 1 || c < 1 || )
+    if (a < 1 || b < 1 || c < 1 )
     {
         return false;
     }
