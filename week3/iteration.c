57691a091096865155e0e287ca74f0b205ca16be
Rain <150204147+Ra1n-42@users.noreply.github.com>
2024-04-22 22:46:14 +0000

    Tue, Apr 23, 2024, 12:46 AM +02:00

--- week3/iteration.c
+++ week3/iteration.c
@@ -12,12 +12,6 @@ int main(void){
 
 void draw(int n)
 {
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < i +1 ; j++)
-        {
-            printf("#");
-        }
-        printf("\n");
+    draw
     }
 }
