c5e67fc92164589fb46d41a035834616952d3eb6
Rain <150204147+Ra1n-42@users.noreply.github.com>
2024-04-22 16:39:08 +0000

    Mon, Apr 22, 2024, 6:39 PM +02:00

--- week2/functions/triangle.c
+++ week2/functions/triangle.c
@@ -10,9 +10,9 @@ int main(void)
 {
     int side_a = 1;
     int side_b = 2;
-    int side_c = 5;
+    int side_c = 10;
 
-    printf("%s\n", valid_triangle(side_a, side_b, side_c));
+    printf("%d\n", valid_triangle(side_a, side_b, side_c));
 }
 
 
@@ -25,7 +25,7 @@ bool valid_triangle(int a, int b, int c)
         return false;
     }
     // calculation
-    return add_two(a, b) > c || add_two(c, b) > a || add_two(a , c) > b;
+    return (add_two(a, b) > c || add_two(c, b) > a || add_two(a , c) > b);
 }
 
 
