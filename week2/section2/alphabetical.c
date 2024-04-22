e6108bf941ee600a5b6105d1341945bb808c56cf
Rain <150204147+Ra1n-42@users.noreply.github.com>
2024-04-22 15:19:31 +0000

    Mon, Apr 22, 2024, 5:19 PM +02:00

---
+++ week2/section2/alphabetical.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    string phrase = get_string("Enter a phrase: ")
+
+    for( int i = 0, length = strlen(phrase); i < length; i++)
+    {
+        
+    }
+}
