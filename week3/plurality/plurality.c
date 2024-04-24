8ef5939b0b5778fb1b458a37a7655a55cef7e6cd
Rain <150204147+Ra1n-42@users.noreply.github.com>
2024-04-24 22:15:17 +0000

    Thu, Apr 25, 2024, 12:15 AM +02:00

--- week3/plurality/plurality.c
+++ week3/plurality/plurality.c
@@ -84,7 +84,7 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    // TODO
+    string winners[candidate_count];
     // Find the maximum number of votes
     for (int i = 0; i < candidate_count; i++)
     {
