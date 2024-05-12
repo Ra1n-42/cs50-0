52fded86337d93be710a1a3a19892917c0eb235d
Rain <150204147+Ra1n-42@users.noreply.github.com>
2024-05-12 14:09:00 +0000

    Sun, May 12, 2024, 4:09 PM +02:00

--- week5/inheritance/inheritance.c
+++ week5/inheritance/inheritance.c
@@ -58,6 +58,7 @@ person *create_family(int generations)
         new_person->parents[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
+        new_person->allels[0] = parent0->alleles[rand() % 2]
     }
 
     // If there are no generations left to create
