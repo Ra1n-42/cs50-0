9c972f710a64b6dccb4a28d8188e65549f393b94
Rain <150204147+Ra1n-42@users.noreply.github.com>
2024-05-12 16:36:40 +0000

    Sun, May 12, 2024, 6:36 PM +02:00

--- week6/dna/dna.py
+++ week6/dna/dna.py
@@ -5,7 +5,7 @@ import sys
 def main():
 
     # TODO: Check for command-line usage
-    print(sys.argv)
+    print(sys.argv[0:-1])
     # TODO: Read database file into a variable
 
     # TODO: Read DNA sequence file into a variable
