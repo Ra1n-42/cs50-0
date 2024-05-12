44aded5f681fa7b865c9c38eb1bc5164bb362b8a
Rain <150204147+Ra1n-42@users.noreply.github.com>
2024-05-12 15:14:48 +0000

    Sun, May 12, 2024, 5:14 PM +02:00

--- week6/sentimental-mario-less/mario.py
+++ week6/sentimental-mario-less/mario.py
@@ -1,7 +1,17 @@
 
 
-height = int(input("Height: "))
 
-while 
-# for i in range(1, height+1):
-#     print(" "*(height-i)+"#"*i)
+while True:
+    height_input = input("Height: ")
+
+
+    if height_input.isdigit():
+        height = int(height_input)
+
+        if 1 <= height <= 8:
+            break
+
+space = height
+
+for i in range(1, height+1):
+    print(" " * (space - i) + "#" * i)
